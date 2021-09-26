a630c3d7a2bb09c6d949c425ffc2c00568c7aa69
Rafał Sztorc <sztorcu18@gmail.com>
2021-09-26 21:38:52 +0200

    using data from datastore

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import styles from './App.scss';
+import List from '../List/List.js';
+import {pageContents, listData} from '../../data/dataStore.js';
+
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>My first React app</h1>
-      </div>
+        <main className = {styles.component}>
+          <h1 className={styles.title}>{pageContents.title}</h1>
+          <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
+          <List {...listData} img='http://uploads.kodilla.com/bootcamp/fer/11.react/space.png' />
+        </main>
     )
   }
 }
-
 export default App;
---
+++ src/components/List/List.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from './List.scss';
+import Hero from '../Hero/Hero.js';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column.js'
+import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
+
+class List extends React.Component {
+    state = {
+    columns: this.props.columns || [],
+  }
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+        description: PropTypes.node,
+        columns: PropTypes.array,
+        img: PropTypes.string.isRequired,
+      }
+    static defaultProps = {
+        description: settings.defaultListDescription,
+}
+  render() {
+    return (
+        <section className = {styles.component}>
+          <Hero titleText = {this.props.title} image={this.props.img} />
+          <div className = {styles.description}>
+              {ReactHtmlParser(this.props.description)}
+          </div>
+          <div className = {styles.columns}>
+          <Column title = 'Animals'/>
+          <Column title = 'Plants'/>
+          <Column title = 'Minerals'/>
+          </div>
+        </section>
+    )
+  }
+}
+
+export default List;
