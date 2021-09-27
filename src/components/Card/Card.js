34496dfb991fb8e7192ce4a22431314cabfcc2d3
Rafał Sztorc <sztorcu18@gmail.com>
2021-09-27 21:39:31 +0200

    Add a Card

---
+++ src/components/Card/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Card.scss';
+import PropTypes from 'prop-types';
+
+class Card extends React.Component {
+
+    render(){
+        return(
+            <main className = {styles.Component}>
+                <h3 className = {styles.title}>
+                    {this.props.title}
+                </h3>
+            </main>
+        )
+    }
+}
+
+export default Card;
